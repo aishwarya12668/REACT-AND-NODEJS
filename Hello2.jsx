4a81a5605f31c27d04f52e61e427df5016e187a4
aishwarya12668 <01fe24bcs259@kletech.ac.in>
2026-06-04 20:57:25 +0530

    Add files via upload

---
+++ Hello2.jsx
@@ -0,0 +1,22 @@
+
+function Hello2(){
+    return <h1>Hello</h1>
+}
+
+
+/*
+function Hello2(props){
+    const {name,message}=props;
+    return <h1>Hello {name} {message}</h1>
+}
+
+
+
+function Hello2(name,message){
+    return <h1>Hello {name} {message}</h1>
+}
+      */
+export default Hello2;
+
+
+//whenever passing arguments pass (props)
